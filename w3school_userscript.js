04ae0aeed2ab094dce0762e90fc306095b062aeb
royaso <zhangroyaso@gmail.com>
2017-10-19 03:11:30 +0800

    header 1 change to red

---
+++ w3school_userscript.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         w3school h1 color to red
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  w3school  html dom  header color change to red
+// @author       royaso
+// @match        http://www.w3school.com.cn/htmldom/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    var maincontent=document.getElementById('maincontent');
+    var header1=maincontent.getElementsByTagName('h1')[0];
+    header1.style.color='red';
+
+
+})();
